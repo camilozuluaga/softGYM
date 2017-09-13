d4fd36d0ee609e3759e0b308a1c80cb11ca63efb
fabiancho32 <the-fabiancho@hotmail.com>
2017-09-12 22:09:01 -0500

    eliminando membresias pagas, con restriccion de caja actual

---
+++ sql/agregando campo membresia_usuario a factura.sql
@@ -0,0 +1,4 @@
+﻿alter table factura add column id_membresia_usuario integer 
+ ALTER TABLE factura ADD CONSTRAINT fk_usuario_membresia
+   FOREIGN KEY (id_membresia_usuario) 
+   REFERENCES "membresia_usuario"(id);
