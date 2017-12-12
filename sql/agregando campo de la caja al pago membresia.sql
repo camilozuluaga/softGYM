0cff4f4e50a9521a3f1d7d4be470b64cacea6f0b
santoescu <santoescu@gmail.com>
2017-12-12 15:31:43 -0500

    subiendo qerys

--- sql/agregando campo de la caja al pago membresia.sql
+++
@@ -1,4 +0,0 @@
-﻿alter table pago_membresia add column id_caja integer ;
-ALTER TABLE pago_membresia ADD CONSTRAINT fk_caja_membre
-   FOREIGN KEY (id_caja) 
-   REFERENCES "caja"(id);
--- sql/agregando campo membresia_usuario a factura.sql
+++
@@ -1,4 +0,0 @@
-﻿alter table factura add column id_membresia_usuario integer 
- ALTER TABLE factura ADD CONSTRAINT fk_usuario_membresia
-   FOREIGN KEY (id_membresia_usuario) 
-   REFERENCES "membresia_usuario"(id);
